1db0bc4ee2b4e6abe6ddea441f5c1c8428f69606
Ludovic Denoyer <ludovic.Denoyer@lip6.fr>
2016-06-20 15:17:01 +0200

    python

--- test.py
+++ test.py
@@ -1,39 +1,44 @@
 import gym
+import lutorpy as lua
 
-import rl.tools as rltools
-import rl.policies as rlpolicies
-import rl.policies.learning as rlpolicieslearning
-import rl.policies.sensors as rlsensors
-import tensorflow as tf
+require('rltorch') 
+require('nn')
+require('dpnn')
 
-env = gym.make('Acrobot-v0')
-sensor=rlsensors.SensorImageToVector(env.observation_space)
+env = gym.make('CartPole-v0')
+print(env.observation_space)
+print(env.action_space)
+action_space=rltorch.Discrete(env.action_space.n)
+observation_space=rltorch.Box(torch.fromNumpyArray(env.observation_space.low),torch.fromNumpyArray(env.observation_space.high))
+policy = rltorch.RandomPolicy(observation_space,action_space)
+sensor=rltorch.BatchVectorSensor(observation_space)
 
-#The model
-model={}
-with tf.variable_scope('my_model') and tf.device('/cpu:0'):
-    STDV=0.01
-    policy_input = tf.placeholder(tf.float32, [None, sensor.size()])
-    w = tf.Variable(tf.random_normal([sensor.size(), env.action_space.n], stddev=STDV))
-    b = tf.Variable(tf.random_normal([env.action_space.n], stddev=STDV))
-    policy_output = tf.matmul(policy_input, w) + b
-    model['input']=policy_input
-    model['output']=policy_output
-    model['init']=tf.initialize_all_variables()
+size_input=sensor._size()
+print(size_input)
+print("Inpust size is %d" % size_input)
+nb_actions=env.action_space.n
+print("Number of actions is %d " % nb_actions)
 
-#The policy
-policy=rlpolicieslearning.DiscretePolicyGradient(env.observation_space,env.action_space,sensor,model,size_batch=5)
+# Creating the policy module
+module_policy=nn.Sequential()._add(nn.Linear(size_input,size_input*2))._add(nn.Tanh())._add(nn.Linear(size_input*2,nb_actions))._add(nn.SoftMax())._add(nn.ReinforceCategorical())
+#module_policy=nn.Sequential():add(nn.Linear(size_input,nb_actions)):add(nn.SoftMax()):add(nn.ReinforceCategorical())
+module_policy._reset(0.001)
 
-for i_episode in range(10):
-    io=env.reset()
-    policy.reset(io)
-    for t in range(1000):
+
+for i_episode in range(20):
+    observation = env.reset()
+    policy._new_episode(torch.fromNumpyArray(observation))
+    for t in range(100):
         env.render()
-        action = policy.sample()
+        print(observation)
+        action = policy._sample()
+        action=action-1
+        #env.action_space.sample()
         observation, reward, done, info = env.step(action)
-        policy.feedback(reward)
-        policy.observe(observation)
+        policy._observe(torch.fromNumpyArray(observation))
+        
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
-    policy.end()
+    policy._end_episode()
+    
